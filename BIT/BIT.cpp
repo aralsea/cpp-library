46de1654b348b3da13be4511df7d00aa3f53878f
Hayato Arai <arai-hayato673@g.ecc.u-tokyo.ac.jp>
2022-11-07 18:24:25 +0900

    BITの配列外参照を修正した

--- BIT/BIT.cpp
+++ BIT/BIT.cpp
@@ -1,55 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define INF 1000000007
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i, n) for (int i = 0; i < n; i++)
+using ll = long long;
+using pii = pair<int, int>;
 
-using pii=pair<int,int>;
-
-//binary indexed tree
-//BIT Zは長さNの配列
-//Z[i]+=x がO(log N)
-//Z[0]+Z[1]+...+Z[k]がO(log N)
-//cf. 普通の配列だとそれぞれO(1),O(N)
-//cf. 累積和だとそれぞれO(N),O(1)
+// binary indexed tree
+// BIT Zは長さNの配列
+// Z[i]+=x がO(log N)
+// Z[0]+Z[1]+...+Z[k]がO(log N)
+// cf. 普通の配列だとそれぞれO(1),O(N)
+// cf. 累積和だとそれぞれO(N),O(1)
 class BIT {
-public:
-    int size_ = 1;
-    vector<int> bit;
+    int _size = 1;
+    vector<ll> bit;
 
-    void init(int sz){
-        size_ = sz + 1;
-        bit.resize(size_, 0);
+   public:
+    BIT(int size) : _size(size) {
+        bit.assign(_size + 1, 0);
     }
 
-    void add(int position, long long x){
-        position++;//1-indexed
-        while(position<=size_){
-            bit[position]+=x;
+    void add(int position, ll x) {
+        position++;  // 1-indexed
+        while (position <= _size) {
+            bit[position] += x;
             position += (position & (-position));
         }
     }
 
-    long long sum(int position){
+    ll sum(int position) {
+        // Z[0]+...+Z[position]
         position++;
-        long long s = 0;
-        while(position>0){
+        ll s = 0;
+        while (position > 0) {
             s += bit[position];
             position -= (position & (-position));
         }
         return s;
     }
 
-
+    ll sum(int left, int right) {
+        // Z[left]+...+Z[right]
+        if (left > right) {
+            return 0;
+        }
+        return sum(right) - sum(left - 1);
+    }
 };
 
-
 int main() {
-    int N=1000;
-    BIT Z;
-    Z.init(N+5);
-    Z.add(1, 15);//Z[1]+=15;
-    cout<<Z.sum(10)<<endl;//Z[0]+...+Z[10]
-    cout<<Z.sum(0)<<endl;//Z[0]
+    int N = 1000;
+    BIT Z(N + 5);
+    Z.add(1, 15);               // Z[1]+=15;
+    cout << Z.sum(10) << endl;  // Z[0]+...+Z[10]
+    cout << Z.sum(0) << endl;   // Z[0]
 
     return 0;
 }
